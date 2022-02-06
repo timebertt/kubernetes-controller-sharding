ff6aaddab2834a7b5be714d6c19ff6988e79a3c9
Tim Ebert <timebertt@gmail.com>
2022-02-07 00:14:03 +0100

    Apply ingress settings in website controller

--- webhosting-operator/main.go
+++ webhosting-operator/main.go
@@ -79,6 +79,7 @@ func main() {
 		Client:   mgr.GetClient(),
 		Scheme:   mgr.GetScheme(),
 		Recorder: mgr.GetEventRecorderFor("website-controller"),
+		Config:   opts.controllerManagerConfig,
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "Website")
 		os.Exit(1)
