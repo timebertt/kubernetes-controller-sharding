de39c022dc3358948d60a2d129d1b617e2ec7748
Tim Ebert <timebertt@gmail.com>
2022-08-07 11:08:00 +0200

    Drop theme controller

--- webhosting-operator/cmd/webhosting-operator/main.go
+++ webhosting-operator/cmd/webhosting-operator/main.go
@@ -86,10 +86,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Website")
 		os.Exit(1)
 	}
-	if err = (&webhosting.ThemeReconciler{}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Theme")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
