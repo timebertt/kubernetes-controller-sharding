b92c8ac2eacc4a3e4bd0043e5ee3ce546e219a6f
Tim Ebert <timebertt@gmail.com>
2023-08-24 14:28:30 +0200

    Don't use webhooks for patching websites in load tests
    Webhooks add a tremendous amount of latency to API calls, especially during load tests when the kyverno admission controller is overloaded.
    High latency of API calls extremely decreases the operator's reconciliation rate which makes it difficult to achieve the desired load.
    We are not load testing kyverno or the API server's webhook machinery, so get that off the critical path.

--- webhosting-operator/pkg/controllers/webhosting/website_controller.go
+++ webhosting-operator/pkg/controllers/webhosting/website_controller.go
@@ -285,6 +285,13 @@ func (r *WebsiteReconciler) IngressForWebsite(serverName string, website *webhos
 	}}
 
 	applyIngressConfigToIngress(r.Config.Ingress, ingress)
+
+	if isGeneratedByExperiment(website) {
+		// don't actually expose website ingresses in load tests
+		// use fake ingress class to prevent overloading ingress controller (this is not what we want to load test)
+		ingress.Spec.IngressClassName = pointer.String("fake")
+	}
+
 	return ingress, ctrl.SetControllerReference(website, ingress, r.Scheme)
 }
 
@@ -397,6 +404,12 @@ func (r *WebsiteReconciler) DeploymentForWebsite(serverName string, website *web
 				},
 			},
 		},
+	}
+
+	if isGeneratedByExperiment(website) {
+		// don't actually run website pods in load tests
+		// otherwise, we would need an immense amount of compute power for running dummy websites
+		deployment.Spec.Replicas = pointer.Int32(0)
 	}
 
 	return deployment, ctrl.SetControllerReference(website, deployment, r.Scheme)
@@ -596,4 +609,8 @@ func GetDeploymentCondition(conditions []appsv1.DeploymentCondition, conditionTy
 		}
 	}
 	return nil
+}
+
+func isGeneratedByExperiment(obj client.Object) bool {
+	return obj.GetLabels()["generated-by"] == "experiment"
 }
